651e2575f96ef7fdfc37d115a9a1f2a0192b7753
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-08-18 16:37:21 +0300

    task 1 update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createImagesCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createImagesCardMarkup(galleryItems) {
+    return galleryItems.map(({original, preview, description}) => {
+        return `
+        <div class="gallery__item">
+            <a href="${original.value}" class="gallery__link">
+                <img 
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                data-source="${original}"
+                ></img>
+            </a>
+        </div>
+        `;
+    }).join('');
+}
+
+const onGalleryContainerClick = (evt) => { 
+    evt.preventDefault();
+
+    if (evt.target.classList.contains("gallery")) return;
+    const source = evt.target.dataset.source;
+    
+    const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
+};
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
 console.log(galleryItems);
