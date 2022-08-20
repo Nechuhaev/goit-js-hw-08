fccf75525ac6308d5939d8f144b59d05bf68ccc3
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-08-20 09:57:06 +0300

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,8 +2,6 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import '../css/common.css';
-import '../css/01-gallery.css';
 
 // Change code below this line
 const galleryContainer = document.querySelector(".gallery");
@@ -14,37 +12,20 @@ galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
 function createImagesCardMarkup(galleryItems) {
     return galleryItems.map(({original, preview, description}) => {
         return `
-        <div class="gallery__item">
-            <a href="${original.value}" class="gallery__link">
+        
+            <a href="${original}" class="gallery__item">
                 <img 
                 class="gallery__image"
                 src="${preview}"
                 alt="${description}"
-                data-source="${original}"
                 ></img>
             </a>
-        </div>
         `;
     }).join('');
 }
-let lightbox = new SimpleLightbox('.gallery a', {
-  scrollZoom: false,
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
   captionDelay: 250,
-  captionsData: 'alt',
-  doubleTapZoom: 1,
+  captionType: "alt",
 });
-
-const onGalleryContainerClick = (evt) => { 
-    evt.preventDefault();
-
-    if (evt.target.classList.contains("gallery")) return;
-    const source = evt.target.dataset.source;
-    
-    const instance = basicLightbox.create(`
-    <img src="${source}"width="800" height="600">`);
-
-  instance.show();
-};
-
-galleryContainer.addEventListener("click", onGalleryContainerClick);
-console.log(galleryItems);
