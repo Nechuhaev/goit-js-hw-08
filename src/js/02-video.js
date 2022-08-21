934e5727d8c50901bc292188e619b6a1626abb89
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-08-21 19:09:34 +0300

    task 3 start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,14 @@ import Player from '@vimeo/player';
 import throttle  from 'lodash.throttle';
 
 const TIME_KEY = 'videoplayer-current-time';
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
 const onPlay = function (data) {
   const strigifyData = JSON.stringify(data);
   localStorage.setItem(TIME_KEY, strigifyData);
 };
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function resumePlayback() {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+
+function onFormSubmit(evt) { }
+
+
+function onTextareaInput(evt) {
+  const value = evt.currentTarget.value;
+
+  console.log(value);
+
+  localStorage.setItem('feedback-form-state', massage)
+}
+
+function pupolateMassageOutput() { }
